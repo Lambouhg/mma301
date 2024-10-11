08b62fb7595b65467d2fc442177f3702a21ae21c
Lambouhg <155502947+Lambouhg@users.noreply.github.com>
2024-10-11 14:51:13 +0700

    Update UserRouter.js

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,7 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const UserController = require("../controllers/UserController");
 
-router.post("/sign-up", UserController.createUser);
-router.post("/", UserController.loginUser);
-module.exports = router;
