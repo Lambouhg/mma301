31ef21780881d66fd609f46e0692d6a384d0db38
hgbao85 <huynhgiabao853@gmail.com>
2024-10-15 16:14:17 +0700

    add-model-user

--- models/User.js
+++ models/User.js
@@ -5,6 +5,9 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
+    phoneNumber: { type: String },
+    address: { type: String },
+    createdAt: { type: Date, default: Date.now },
     orders: [{
         orderId: String,
         status: String,
