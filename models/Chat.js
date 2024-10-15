663e6b60b8644e8fb05175e88835b0f550457179
Lambouhg <155502947+Lambouhg@users.noreply.github.com>
2024-10-15 17:03:26 +0700

    Update Chat.js

--- models/Chat.js
+++ models/Chat.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
-    sender: { type: String, required: true },
+ username: { type: String, required: true },
     message: { type: String, required: true },
     timestamp: { type: Date, default: Date.now }
 });
